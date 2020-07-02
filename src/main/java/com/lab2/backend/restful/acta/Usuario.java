3cffe8b4390a1f6115cf5ca26efdf5a0ad5ff7aa
Anyi <anyeliv02@gmail.com>
2020-07-02 00:50:15 -0400

    reestructuracion del codigo + comenzando con autenticacion

---
+++ src/main/java/com/lab2/backend/restful/acta/Usuario.java
@@ -0,0 +1,27 @@
+package com.lab2.backend.restful.acta;
+
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+
+@Data
+public class Usuario {
+    // @Id
+    // @GeneratedValue
+    // private String codigo;
+
+    // private String nombre;
+
+    // private String apellido;
+
+
+}
